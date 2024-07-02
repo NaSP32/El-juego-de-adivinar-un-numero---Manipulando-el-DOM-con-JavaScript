07ab7cab8f0bb5162d9196e28e9a1eb77562e1d0
Nadia Soledad Pereyra <nadiapereyra32@gmail.com>
2024-07-02 08:57:55 -0300

    Manipulando objetos del DOM de HTML a traves de JavaScript

---
+++ app.js
@@ -0,0 +1,7 @@
+//genero una variable que es un objeto con el querySelector manipulo los objetos del DOM
+let titulo = document.querySelector('h1');
+//coloco un texto, le defino un titulo:
+titulo.innerHTML = 'Juego del número secreto';
+//genero una variable para el objeto parrafo:
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Ingresa un número del 1 al 10';
