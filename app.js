c56aa24e7d9d33c12a7b75a94cceaae3d9c3b9d1
Nadia Soledad Pereyra <nadiapereyra32@gmail.com>
2024-07-05 17:36:27 -0300

    Creo la funcion verificarIntento que compara el número que ingresa el usuario a traves del input, con el numero generado por el programa y devuelve una respuesta

--- app.js
+++ app.js
@@ -6,14 +6,43 @@
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'Ingresa un número del 1 al 10';
 */
-//Definimos y declaramos una funcion
-function intentoDeUsuario() {
-    alert('Click desde el boton');
-}
+
+let numeroSecreto = generarNumeroSecreto();
 
 function asignarElementoTexto(elemento, texto) {
     let titulo = document.querySelector(elemento);
     titulo.innerHTML = (texto);
+    return;
 }
+
+//Definimos y declaramos una funcion para verificar los intentos
+function verificarIntento() {
+    //la funcion getById me permite buscar elementos por id .value trae el valor que tiene ese elemento
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+       
+    console.log(numeroSecreto);
+    //=== compara numero y tipo de dato
+    if (numeroDeUsuario === numeroSecreto) {
+        //llamo a la funcion ElementoTexto para cambiar el mensaje
+        asignarElementoTexto( 'p' , 'Acertaste el número ');
+    } else {
+        if(numeroDeUsuario > numeroSecreto){
+            asignarElementoTexto('p', 'El número que yo pense es menor')
+
+        }else{
+            asignarElementoTexto('p', 'El número que yo pense es mayor')
+
+        }
+    }
+    return;
+}
+
+//Creo una funcion para generar un número aleatorio entre 1 y 100
+function generarNumeroSecreto() {
+    return Math.floor(Math.random() * 100)+ 1;
+    
+}
+
+//Llamo a la funcion asignarElementoTexto para crear los mensajes en pantalla
 asignarElementoTexto('h1', "Bienvenid@ al juego del número secreto");
 asignarElementoTexto('p', 'Ingresa un número del 1 al 100');
